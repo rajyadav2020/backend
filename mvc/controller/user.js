5cb585b701234c5c543d4cf8664bc256fe510631
rajyadav2020 <rajyadavv176@gmail.com>
2026-07-13 08:13:37 +0530

    create read update delete operations have been done

--- mvc/controller/user.js
+++ mvc/controller/user.js
@@ -1,4 +1,6 @@
+
 const User = require("../models/user")
+
 
 async function handlegetallusers(req,res){
   const alldbusers = await User.find({});
@@ -11,11 +13,70 @@ async function getuserbyid(req,res){
   return res.json(user)
 }
 
-async function handleupdate(req,res){
-  
+async function handleUpdate(req, res) {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
+async function deleteUser(req, res) {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+
+    if (!deletedUser) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    return res.status(200).json({
+      message: "User deleted successfully",
+      user: deletedUser,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+}
+
+async function newuser(req,res)
+{
+  const body = req.body;
+  if(
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title
+  ) {
+    return res.status(400).json({msg :"all fields are requ...."})
+  }
+
+  const result = await User.create({
+    firstname: body.first_name,
+    lastname: body.last_name,
+    email: body.email,
+    gender : body.gender,
+    jobtitle:body.job_title
+  })
+
+  return res.status(201).json({msg:"success"})
 }
 
 module.exports = {
   handlegetallusers,
-  getuserbyid
+  getuserbyid,
+  handleUpdate,
+  deleteUser,
+  newuser
 }
--- mvc/routes/user.js
+++ mvc/routes/user.js
@@ -3,48 +3,16 @@ const { model } = require('mongoose');
 
 const router = express.Router();
 const User = require("../models/user")
-const {handlegetallusers,getuserbyid} = require("../controller/user")
+const {handlegetallusers,getuserbyid,handleUpdate,deleteUser,newuser} = require("../controller/user")
 
-router.get("/",handlegetallusers)
 
+router.get("/", handlegetallusers);
 
 router
-  .route("./:id", getuserbyid)
+  .get("/:id", getuserbyid)
+  .patch("/:id", handleUpdate)
+  .delete("/:id", deleteUser);
 
+router.post("/", newuser);
 
-  .patch(async (req,res)=>{
-    await User.findByIdAndUpdate(req.params.id, {lastname: "changed"});
-    return res.json({status:"success"});
-  })
-
-  .delete(async (req,res)=>{
-    await User.findByIdAndDelete(req.params.id)
-    return res.json({status: "success"})
-  })
-
-
-router.post("/", async (req,res)=>{
-  const body = req.body;
-  if(
-    !body ||
-    !body.first_name ||
-    !body.last_name ||
-    !body.email ||
-    !body.gender ||
-    !body.job_title
-  ) {
-    return res.status(400).json({msg :"all fields are requ...."})
-  }
-
-  const result = await User.create({
-    firstname: body.first_name,
-    lastname: body.last_name,
-    email: body.email,
-    gender : body.gender,
-    jobtitle:body.job_title
-  })
-
-  return res.status(201).json({msg:"success"})
-})
-
-module.exports = router
+module.exports = router;
